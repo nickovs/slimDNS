da92879841227772f654b9390536a7ae9b200e41
Dhanar Adi Dewandaru <dhanar.adi@formulatrix.com>
2020-03-22 13:15:15 +0700

    Separate command line wrapper from the library

---
+++ slimDNSApp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+import slimDNS
+
+if __name__ == __main__
+    if len(sys.argv)  3
+        print(Usage {} local_addr local_host.format(sys.argv[0]))
+        sys.exit(1)
+    local_addr = sys.argv[1]
+    local_host = sys.argv[2]
+    server = slimDNS.SlimDNSServer(local_addr, local_host)
+    server.run_forever()

57d53fb39b9cc853231cbab64af4ee212ba62d70
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2020-03-22 20:29:14 +0700

    Fix typo in command line wrapper

--- slimDNSApp.py
+++ slimDNSApp.py
@@ -3,8 +3,8 @@
 import sys
 import slimDNS
 
-if __name__ == __main__
-    if len(sys.argv)  3
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
         print(Usage {} local_addr local_host.format(sys.argv[0]))
         sys.exit(1)
     local_addr = sys.argv[1]
